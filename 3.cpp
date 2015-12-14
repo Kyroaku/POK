8c3e39572efd52be7e0714adb6fed012c15e9982
Kyroaku <volffikk@gmail.com>
2015-12-14 16:30:53 +0100

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -3,13 +3,14 @@
 
 void func(int M[20][20], int N, int R[20][20])
 {
-	for(int i = 0; i < N; i++)
+	for(int i = 0; i < N; i++) /* dla kazdego wiersza */
 	{
-		for(int j = 0 ; j < N; j++)
+		for(int j = 0 ; j < N; j++) /* dla kazdej skladowej wiersza i */
 		{
-			R[i][j] = M[j][i];
+			R[i][j] = M[j][i]; /* zamieniamy wiersze z kolumnami i zapisujemy wynik do tablicy R */
 		}
 	}
+	/* R jest wskaznikiem (poniewaz tablica to wskaznik), a wiec zmiany beda "zapamietane" na zewnatrz funkcji */
 }
 
 int main()
@@ -21,20 +22,22 @@ int main()
 	printf("Podaj N: ");
 	scanf("%d", &N);
 	
+	/* wypelniamy macierz losowymi wartosciami z zakresu 0-9 i przy okazji wyswietlamy macierz na ekranie */
 	for(int i = 0; i < N; i++) {
 		for(int j = 0 ; j < N; j++) {
 			M[i][j] = rand()%10;
 			printf("%d ", M[i][j]);
 		}
-		printf("\n");
+		printf("\n"); /* po wypisaniu calego wiersza trzeba przejsc do kolejnej linijki */
 	}
 	printf("\n\n");
 	
+	/* Jako argument podajemy tablice R, w ktorej zapisana zostanie transponowana macierz M */
 	func(M, N, R);
 	
+	/* podobnie, jak wczesniej, wypisujemy macierz na ekran. Tym razem macierz R */
 	for(int i = 0; i < N; i++) {
 		for(int j = 0 ; j < N; j++) {
-			M[i][j] = rand()%10;
 			printf("%d ", R[i][j]);
 		}
 		printf("\n");
